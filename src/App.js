1e246da259e2a5add5d98129f9524fd1736a3194
orhankalayci-devopscu <orhan@kalayci.com>
2021-03-04 16:26:18 +0300

    Revert "make it fail the test (learn -> earn)"
    This reverts commit 730d915c812785bed905aa43f31b0ce764e0d02a.

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          earn react 
+          learn react 
         </a>
       </header>
     </div>
